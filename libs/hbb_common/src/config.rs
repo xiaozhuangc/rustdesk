411644e526589e308b465d95846a04fc810e131f
xiaozhuangc <68954057+xiaozhuangc@users.noreply.github.com>
2024-11-21 23:20:30 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["xz.meeuu.com:"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xz.meeuu.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "oL36EtTynGbeL3oFxenEupBYHZb5KIMLA6XfS5btDhw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
