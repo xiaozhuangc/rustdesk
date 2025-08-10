91a2655089902caffaacd4d9cd763dc6f99864a6
xiaozhuangc <68954057+xiaozhuangc@users.noreply.github.com>
2025-08-11 02:16:35 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://desk.meeuu.com:8443".to_owned()
+    "https://desk.meeuu.com:21114".to_owned()
 }
 
 #[inline]
